65e28c9d120e3848b27026e7567319853724e079
chohuison <117262031+chohuison@users.noreply.github.com>
2024-04-11 23:13:07 +0900

    hospitalList

---
+++ pettopia_front/lib/hospital/page/hospitalList.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:pettopia_front/hospital/page/hospitalValue.dart';
+
+class HospitalList extends StatefulWidget{
+  final List<Map<String, dynamic>>hospitalList;
+  const HospitalList({
+    Key? key,
+   required this.hospitalList,
+  }) : super(key: key,);
+
+  @override
+  _HospitalListState createState() => _HospitalListState();
+
+}
+
+class _HospitalListState extends State<HospitalList> {
+ 
+  @override
+  Widget build(BuildContext context) {
+    return  ListView.builder(
+      shrinkWrap: true,
+       physics: NeverScrollableScrollPhysics(),
+          itemCount: widget.hospitalList.length,
+        itemBuilder: (BuildContext context, int index) {
+  if (widget.hospitalList != null && index < widget.hospitalList.length) {
+    final record =widget. hospitalList[index]!;
+    return HospitalValue(
+      value: record,
+    );
+  } else {
+    // hospitalList가 null이거나 index가 범위를 벗어나는 경우에 대한 처리
+    return SizedBox(); // 혹은 다른 처리를 수행할 수 있음
+  }
+},
+        );
+  }
+}
--- pettopia_front/lib/hospital/page/hospitalSearch.dart
+++ pettopia_front/lib/hospital/page/hospitalSearch.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:pettopia_front/Menu/CustomBottomNavigatorBar.dart';
+import 'package:pettopia_front/hospital/page/hospitalList.dart';
+import 'package:pettopia_front/hospital/page/hospitalValue.dart';
 import 'package:pettopia_front/hospital/widget/hospitalAppbar.dart';
 import 'package:pettopia_front/hospital/widget/regionSelectBox.dart';
 
@@ -13,14 +15,25 @@ class HospitalSearch extends StatefulWidget {
 }
 
 class _HospitalSearchState extends State<HospitalSearch> {
+  late List<Map<String, dynamic>>? hospitalList;
+
+@override
+void initState() {
+  super.initState();
+  //연결하면서 병원 종류 가져오기 
+  hospitalList ??= [
+    {'name': '구미옥계동물병원', 'address': '경북 구미시 산호대로 31길 10', 'phone': '0507-****-****'},
+    {'name': '구미옥계동물병원', 'address': '경북 구미시 산호대로 31길 10', 'phone': '0507-****-****'}
+  ];
+}
+
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize: const Size(411.4, 683.4), // 디자인 기준 크기를 설정합니다.
+      designSize: const Size(411.4, 683.4),
       child: MaterialApp(
         title: "hospitalSearch",
         builder: (context, child) {
-          // MediaQuery를 사용하여 텍스트 크기 조정을 비활성화합니다.
           return MediaQuery(
             data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
             child: child!,
@@ -28,36 +41,37 @@ class _HospitalSearchState extends State<HospitalSearch> {
         },
         home: Scaffold(
           backgroundColor: Color.fromRGBO(237, 237, 233, 1.0),
-          body: ListView(
-            children: [
-              HospitalAppBar(page: 0),
+        body: Column(
+          children:<Widget>[
+            HospitalAppBar(page: 0),
               Container(
                 height: 485.h,
-                  margin: EdgeInsets.symmetric(vertical: 1.0.h, horizontal: 20.0.w),
+                margin: EdgeInsets.symmetric(vertical: 1.0.h, horizontal: 20.0.w),
                 decoration: BoxDecoration(
                   color: Color(0xFFD5BDAF),
-                   borderRadius: BorderRadius.circular(25),
+                  borderRadius: BorderRadius.circular(25),
                 ),
-                child:Column(
-                  children:<Widget>[
+                 child: Column(
+                  children: <Widget>[
                     Container(
-                         alignment: Alignment.centerLeft,
-                    child:RegionSelectBox(),
-                       padding: EdgeInsets.fromLTRB(20.w, 10.h, 0.w, 10.h),
-                             
-                              height:50.h),
-                    Container(
-                 
-                      // color:Colors.blue,
-                    
-                      height:405.h
-                    )
+                      alignment: Alignment.centerLeft,
+                      child: RegionSelectBox(),
+                      padding: EdgeInsets.fromLTRB(20.w, 10.h, 0.w, 10.h),
+                      height: 50.h,
+                    ),
+                     HospitalList(hospitalList: hospitalList!,)
                   ],
-
-                )
-              )
+                ),
+              ),
+             
             ],
-          ),
+
+              ),
+             
+
+          
+
+      
           bottomNavigationBar: CustomBottomNavigatorBar(page: 1),
         ),
       ),
