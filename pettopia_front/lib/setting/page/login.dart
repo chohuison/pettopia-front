c428357d7bb64d6b856b36f4244408617307daee
A-Yeong <dkdud064@kumoh.ac.kr>
2024-05-30 00:09:42 +0900

    petFilter

--- pettopia_front/lib/setting/page/login.dart
+++ pettopia_front/lib/setting/page/login.dart
@@ -17,125 +17,122 @@ class Login extends StatefulWidget {
 
 class _LoginState extends State<Login> {
   final userServer = Users();
-  late WebViewController _webViewController= WebViewController();
+  late WebViewController _webViewController = WebViewController();
   final FlutterSecureStorage _secureStorage = FlutterSecureStorage();
-  late String _serverUrl="";
+  late String _serverUrl = "";
 
- @override
-void initState() {
-  super.initState();
-  // _initializeWebview();
-}
+  @override
+  void initState() {
+    super.initState();
+    // _initializeWebview();
+  }
 
-void _kakaoWebViewSetting() async {
-  await _getServerUrl();
-  String url = _serverUrl + "oauth2/authorization/kakao";
+  void _kakaoWebViewSetting() async {
+    await _getServerUrl();
+    String url = _serverUrl + "oauth2/authorization/kakao";
     // String url = 'http://10.0.2.2/' + "oauth2/authorization/kakao";
-  print(url);
-  _webViewController = WebViewController()
-    ..setJavaScriptMode(JavaScriptMode.unrestricted)
-    ..loadRequest(Uri.parse(url))
-    ..setNavigationDelegate(NavigationDelegate(
-      onNavigationRequest: (NavigationRequest request) {
-        print("Navigating to: ${request.url}");
-        return NavigationDecision.navigate;
-      },
-      onPageFinished: (String url) {
-        _webViewController
-            .runJavaScriptReturningResult("document.body.outerHTML")
-            .then((html) {
-          print("Page finished loading. URL: $url");
-          if (url.contains("code=")) {
-            print("OAuth code found in URL:");
-            print(html);
+    print(url);
+    _webViewController = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse(url))
+      ..setNavigationDelegate(NavigationDelegate(
+        onNavigationRequest: (NavigationRequest request) {
+          print("Navigating to: ${request.url}");
+          return NavigationDecision.navigate;
+        },
+        onPageFinished: (String url) {
+          _webViewController
+              .runJavaScriptReturningResult("document.body.outerHTML")
+              .then((html) {
+            print("Page finished loading. URL: $url");
+            if (url.contains("code=")) {
+              print("OAuth code found in URL:");
+              print(html);
 
-            print(html.runtimeType);
-            String strHtml = html as String;
-            split(strHtml);
-            // return NavigationDecision.prevent;
+              print(html.runtimeType);
+              String strHtml = html as String;
+              split(strHtml);
+              // return NavigationDecision.prevent;
 
-            // MyApp 또는 원하는 페이지로 이동
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => MyApp()), // 여기에 원하는 페이지를 넣으세요
-            );
-          }
-        }).catchError((error) {
-          print("Error getting HTML: $error");
-        });
-      },
-    ));
-}
+              // MyApp 또는 원하는 페이지로 이동
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => MyApp()), // 여기에 원하는 페이지를 넣으세요
+              );
+            }
+          }).catchError((error) {
+            print("Error getting HTML: $error");
+          });
+        },
+      ));
+  }
 
   Future<void> _getServerUrl() async {
-  try {
-    await dotenv.load(fileName: ".env");
-  } catch (e) {
-    print("Error loading .env file: $e"); // 오류 메시지 출력
+    try {
+      await dotenv.load(fileName: ".env");
+    } catch (e) {
+      print("Error loading .env file: $e"); // 오류 메시지 출력
+    }
+    _serverUrl = dotenv.env['DB_SERVER_URI'] ?? 'YOUR_KAKAO_APP_KEY';
+    print(_serverUrl);
   }
-  _serverUrl = dotenv.env['DB_SERVER_URI'] ?? 'YOUR_KAKAO_APP_KEY';
-  print(_serverUrl);
-}
-  
-  void split(String html){
+
+  void split(String html) {
     print("넘어온 html");
     print(html);
-   RegExp jsonPattern = RegExp(r'\{.*\}', dotAll: true); // 중괄호 포함 부분 추출
-  Iterable<Match> matches = jsonPattern.allMatches(html); // 모든 일치 항목 찾기
+    RegExp jsonPattern = RegExp(r'\{.*\}', dotAll: true); // 중괄호 포함 부분 추출
+    Iterable<Match> matches = jsonPattern.allMatches(html); // 모든 일치 항목 찾기
 
-  if (matches.isNotEmpty) {
-    String jsonString = matches.first.group(0)!; // 첫 번째 일치 항목 추출
-    print("Extracted JSON: $jsonString");
-    String jsonStr = jsonString.replaceAll(r'\', '');
+    if (matches.isNotEmpty) {
+      String jsonString = matches.first.group(0)!; // 첫 번째 일치 항목 추출
+      print("Extracted JSON: $jsonString");
+      String jsonStr = jsonString.replaceAll(r'\', '');
 
-    String accessToken="";
-    String refreshToken="";
-     final parts = jsonStr.split('"');
-     for(int i=0; i< parts.length;i++){
-      print(parts[i]);
-     }
-   for (int i = 0; i < parts.length; i++) {
-      if (parts[i] == 'accessToken') {
-        accessToken = parts[i + 2]; // 값이 따옴표 사이에 있기 때문
-      } else if (parts[i] == 'refreshToken') {
-        refreshToken = parts[i + 2];
+      String accessToken = "";
+      String refreshToken = "";
+      final parts = jsonStr.split('"');
+      for (int i = 0; i < parts.length; i++) {
+        print(parts[i]);
       }
+      for (int i = 0; i < parts.length; i++) {
+        if (parts[i] == 'accessToken') {
+          accessToken = parts[i + 2]; // 값이 따옴표 사이에 있기 때문
+        } else if (parts[i] == 'refreshToken') {
+          refreshToken = parts[i + 2];
+        }
+      }
+      saveToken(accessToken, refreshToken);
+
+      print("Access Token: $accessToken");
+      print("Refresh Token: $refreshToken");
+    } else {
+      print("No JSON object found.");
     }
-    saveToken(accessToken, refreshToken);
+  }
 
-
-    print("Access Token: $accessToken");
-    print("Refresh Token: $refreshToken");
-
-  } else {
-    print("No JSON object found.");
-  }}
-
-  Future<void> saveToken(String accessToken, String refreshToken) async{
+  Future<void> saveToken(String accessToken, String refreshToken) async {
     await _secureStorage.deleteAll();
 
     await _secureStorage.write(key: 'accessToken', value: accessToken);
-    await _secureStorage.write(key: 'refreshToken', value:refreshToken);
+    await _secureStorage.write(key: 'refreshToken', value: refreshToken);
   }
 
   void launchLogin() {
-   
-  
-        Navigator.of(context).push(
+    Navigator.of(context).push(
       MaterialPageRoute(
-        builder: (context) => WebViewPage( controller: _webViewController),
+        builder: (context) => WebViewPage(controller: _webViewController),
       ),
     );
   }
 
- 
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize: const Size(411.42857142857144,683.4285714285714),
+      designSize: const Size(411.42857142857144, 683.4285714285714),
       builder: (_, __) {
         return MaterialApp(
-             debugShowCheckedModeBanner: false ,
+          debugShowCheckedModeBanner: false,
           title: "Login",
           home: Scaffold(
             resizeToAvoidBottomInset: false,
@@ -167,22 +164,23 @@ void _kakaoWebViewSetting() async {
                     thickness: 1,
                     color: Color(0xFFD5BDAF),
                   ),
-                 Container(
-    width: 185.w,
-    height: 50.h,
-    child: IconButton(
-      onPressed:(){
-        print("버튼 누름");
-        _kakaoWebViewSetting();
-        launchLogin();} , // 함수 호출
-      icon: Image.asset(
-        'assets/img/kakao_login.png',
-        width: double.infinity,
-        height: double.infinity,
-        fit: BoxFit.cover,
-      ),
-    ),
-  ),
+                  Container(
+                    width: 185.w,
+                    height: 50.h,
+                    child: IconButton(
+                      onPressed: () {
+                        print("버튼 누름");
+                        _kakaoWebViewSetting();
+                        launchLogin();
+                      }, // 함수 호출
+                      icon: Image.asset(
+                        'assets/img/kakao_login.png',
+                        width: double.infinity,
+                        height: double.infinity,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
                   _naverLogin(),
                   _googleLogin(),
                 ],
@@ -236,17 +234,15 @@ Widget _googleLogin() {
     height: 50.h,
     child: IconButton(
       onPressed: () {},
-      icon: Image.asset(
-        'assets/img/google_login.png'),
+      icon: Image.asset('assets/img/google_login.png'),
     ),
   );
 }
 
 class WebViewPage extends StatefulWidget {
-
   final WebViewController controller;
 
-  WebViewPage({ required this.controller});
+  WebViewPage({required this.controller});
 
   @override
   _WebViewPageState createState() => _WebViewPageState();
